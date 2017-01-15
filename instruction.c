d2d65335d4d7aa14ada739667fbfc7f58b175805
Skye M <skyem@hotmail.co.uk>
2017-01-15 21:41:21 +0000

    Implemented PUSH SR

--- instruction.c
+++ instruction.c
@@ -1,9 +1,13 @@
 #include "instruction.h"
+#include "register.h"
 
-void NOP(BYTE f) {
-	return;
-}
+// NOP - Do nothing.
+void NOP(BYTE f) {}
 
+// PUSH_SR, push the status register to the stack.
+// (Decrements xsp by 2, then places the 2 byte status register at the "top" of the stack).
+// (-XSP) <- SR
 void PUSH_SR(BYTE f) {
-	 
+	cpu_stack_push_w(CPU_STATE.SR);
 }
+
